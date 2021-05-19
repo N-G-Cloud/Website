a86fe5bdce1057425a839dac0756e519d36885e6
Cameron Steele <cam.steeleis@gmail.com>
2021-05-19 15:39:22 -0500

    Fixed deprecated plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    //"gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
